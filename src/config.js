e4e544b4e6d3eb851b74a26e1fe84d770aadcfd1
Tyltin Anton <quasardevelopers@gmail.com>
2022-03-26 13:02:50 +0200

    QD

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: 'http://localhost',
-    port: 4050,
+    port: 4500,
     logins: [
         {login: "user1", pwd: "qwerty"},
         {login: "user2", pwd: "qwerty"},
