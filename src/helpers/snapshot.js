80ff41e1d69644d9c0b2b4ca6a04ce1b89a35e5f
Tyltin Anton <quasardevelopers@gmail.com>
2022-03-28 10:28:40 +0300

    QD

--- src/helpers/snapshot.js
+++ src/helpers/snapshot.js
@@ -1,5 +1,4 @@
-//const Pageres = require('pageres');
-const capture = require('capture-website');
+const Pageres = require('pageres');
 module.exports = async (options)=>{
     let size = options.size || '1280x1024';
     let full = options.full || false;
@@ -9,16 +8,12 @@ module.exports = async (options)=>{
     let filename = options.url.replace(/[\/:.]/g, '');
 
 
-    await capture.file('<h1>Awesome!</h1>', 'screenshot.png', {
-        inputType: 'html'
+    let page = new Pageres({
+        delay: delay,
+        crop: !full,
+        filename: filename,
+        format: 'png'
     });
-
-    // let page = new Pageres({
-    //     delay: delay,
-    //     crop: !full,
-    //     filename: filename,
-    //     format: 'png'
-    // });
-    // await page.src(options.url, [size]).dest(__dirname + '/../../public/images').run();
+    await page.src(options.url, [size]).dest(__dirname + '/../../public/images').run();
     return filename;
 }
